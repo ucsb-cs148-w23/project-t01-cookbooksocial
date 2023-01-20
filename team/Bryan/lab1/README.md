84d5fd19674a689e9e5c6662e3ece51fe8894c92
bzamora020 <bryan.aldair.zamora@gmail.com>
2023-01-20 11:01:47 -0800

    Bryan folder structure changed

---
+++ team/Bryan/lab1/README.md
@@ -0,0 +1,13 @@
+# Hello World App for lab 1
+
+
+## To run the app
+
+
+cd into the hello-world folder, then type the following commands:
+
+1: `npm install`
+
+2: `npm start`
+
+Then the app will run in your port 3000
---
+++ team/Bryan/lab1/hello-world/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World app for CS 148!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
