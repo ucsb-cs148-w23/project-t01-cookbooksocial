0600610654de842f113efaecc1e35dce1df50682
Nicolas Johnson <njohnson@ucsb.edu>
2023-01-26 16:09:18 -0800

    nj - clear css and html from frontend homepage

--- CookBookSocial/frontend/src/App.js
+++ CookBookSocial/frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const [response, setResponse] = useState(null)
@@ -25,12 +24,9 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         {response}
       </header>
-    </div>
   );
 }
 
