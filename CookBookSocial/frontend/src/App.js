f6a79af088dc5df8d344055d2b1854fa186017d1
bzamora020 <bryan.aldair.zamora@gmail.com>
2023-01-27 01:24:59 -0800

    bz - added navbar and changed the browser router to index.js

--- CookBookSocial/frontend/src/App.js
+++ CookBookSocial/frontend/src/App.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react';
-import ExamplePage from './pages/ExamplePage.js';
+import React, { useState } from "react";
+import ExamplePage from "./pages/ExamplePage.js";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Navbars from "./components/navbars/Navbars.jsx";
 
 function App() {
-  return(
-    <Router>
+  return (
+    <>
+      {/* We import the navbars component here because we want it to be used on the whole app */}
+      <Navbars />
       <Routes>
-
         <Route exact path="/" element={<ExamplePage />} />
-
       </Routes>
-    </Router>
-  )
+    </>
+  );
 }
 
 export default App;
