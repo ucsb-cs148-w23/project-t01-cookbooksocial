55d55cced72a95568cbf95781bb53251b748756d
Terry Pi <tai-jui@umail.ucsb.edu>
2023-01-27 11:22:10 -0800

    tp - changed wording from profil to login

--- CookBookSocial/frontend/src/components/navbars/Navbars.jsx
+++ CookBookSocial/frontend/src/components/navbars/Navbars.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function Navbars() {
+  return (
+    // Navbar object wrapper as a whole
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/home">CookBook Social</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {/* Setting links for navbar */}
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="profile">Profile</Nav.Link>
+            <Nav.Link href="login">Login</Nav.Link>
 
-    return (
-      // Navbar object wrapper as a whole 
-        <Navbar bg="dark" variant = "dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="/home">CookBook Social</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-
-              {/* Setting links for navbar */}
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="profile">Profile</Nav.Link>
-              
-              {/* Dropdown customization */}
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-    )
+            {/* Dropdown customization */}
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
