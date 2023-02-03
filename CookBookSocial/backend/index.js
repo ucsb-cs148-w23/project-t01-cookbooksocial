9c9d28c0d640c5af6d40d2a7915472685bb062b5
Nicolas Johnson <njohnson@ucsb.edu>
2023-02-02 16:33:21 -0800

    nj - attempt to make app deployable on Heroku

--- CookBookSocial/backend/index.js
+++ CookBookSocial/backend/index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 
 
 // Every time frontend tries to serve this (backend), it will automatically be stored in public folder.
-// app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname + "../frontend/build")));
 
 // For later setting environment variables, else we use 3001 as default port.
 const port = process.env.PORT || 3001;
@@ -17,7 +17,7 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
+app.get('/api/exampletest', (req, res) => {
     console.log("Req found");
     res.json({info:'Hello World'} || {});
 });
@@ -25,3 +25,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example Express app listening at http://localhost:${port}`)
 });
+
+
+//Anything that does not match the above api routes will be rerouted to the static pages (frontend)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  })
