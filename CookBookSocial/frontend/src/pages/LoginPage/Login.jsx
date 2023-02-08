bbcf8bd9834fe4e24f79ba952f48e4e6a1db55f4
Jacob <36573259+Jacob10383@users.noreply.github.com>
2023-02-07 20:18:28 -0800

    Update Login.jsx
    Remove unused import from prev merge

--- CookBookSocial/frontend/src/pages/LoginPage/Login.jsx
+++ CookBookSocial/frontend/src/pages/LoginPage/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect } from "firebase/auth";
 import { useAuth } from "../../contexts/AuthContext";
 import styles from "./Login.module.css";
 
