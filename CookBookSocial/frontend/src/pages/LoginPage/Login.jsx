7d2894ba370b78515f50a1e8c60709b3ad0d69fb
Jacob Toomey <jtoomey@ucsb.edu>
2023-02-19 00:46:06 -0800

    Update signinpage

--- CookBookSocial/frontend/src/pages/LoginPage/Login.jsx
+++ CookBookSocial/frontend/src/pages/LoginPage/Login.jsx
@@ -5,77 +5,74 @@ import { useAuth } from "../../contexts/AuthContext";
 import styles from "./Login.module.css";
 
 export default function Login() {
+  const { currentUser, login, setError, loginWithGoogle } = useAuth();
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const { currentUser, login, setError, loginWithGoogle } = useAuth();
+
   useEffect(() => {
-    if (currentUser) {
-      console.log("User detected")
-      navigate("/home");
-    }
+    currentUser && navigate("/home");
   }, [currentUser, navigate]);
 
-  async function handleFormSubmit(e) {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+    setLoading(true);
     try {
-      setError("");
-      setLoading(true);
       await login(email, password);
       navigate("/home");
     } catch (e) {
       setError("Failed to login");
     }
-
     setLoading(false);
-  }
-  const auth = getAuth();
-  const provider = new GoogleAuthProvider();
-  async function GoogleRedrct() {
-    signInWithRedirect(auth, provider);
-  }
+  };
+
+  const GoogleRedirect = () => signInWithRedirect(auth, provider);
 
   return (
-    <div className="">
-      <div className="">
+    <div className={styles.container}>
+      <div className={styles.rectangle}>
         <div className={styles.topText}>
           <h2>Login to your account</h2>
         </div>
         <form onSubmit={handleFormSubmit}>
           <div>
             <div className={styles.inputFields}>
+              <label htmlFor="email-address">Email address</label>
               <input
                 id="email-address"
                 name="email"
                 type="email"
                 autoComplete="email"
                 required
-                className=" px-3 py-2 border border-gray-300"
-                placeholder="Email address"
+                className={styles.input}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
-            <div className={styles.inputFields}>
+            <div className={styles["Login-input"]}>
+              <label htmlFor="password">Password</label>
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="px-3 py-2 border border-gray-300"
-                placeholder="Password"
+                className={styles.input}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
 
-          <div className={styles.inputFields}>
+          <div className={styles["Login-input"]}>
             <button
               type="submit"
               disabled={loading}
-              className="py-2 px-4 border border-transparent "
+              className={styles["Login-button"]}
             >
               Login
             </button>
@@ -83,18 +80,18 @@ export default function Login() {
         </form>
 
         <div className={styles.inputFields}>
+          <div className={styles.orText}>Or sign in with:</div>
           <button
-            onClick={GoogleRedrct}
+            onClick={GoogleRedirect}
             disabled={loading}
-            className="py-2 px-4 border border-transparent "
+            className={styles.googleButton}
           >
-            Login With Google
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google logo" className={styles.googleIcon} />
           </button>
         </div>
-
         <div className="flex items-center justify-between">
           <div className="text-sm">
-            <Link to="/register" className="text-blue-600 ">
+            <Link to="/register" className="text-blue-600">
               Don't have an account? Register
             </Link>
           </div>
