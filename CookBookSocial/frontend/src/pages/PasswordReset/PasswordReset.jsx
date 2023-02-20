087626797cdd388b9145c3a3502963c1ca0af7f2
Jacob Toomey <jtoomey@ucsb.edu>
2023-02-19 20:26:44 -0800

    Update passwordreset
    Upon pressing submit, the submit button disappears. If email sending was successful then the input box becomes inaccessible as well. If the email was not sent, then the error message will disappear and the submit button will reappear upon the user typing into the input box. Also, the code now handles too many password attempt errors aswell as blanket handling of any other error. Also adding spinning icon during the 5 second countdown to redirect to login page.

--- CookBookSocial/frontend/src/pages/PasswordReset/PasswordReset.jsx
+++ CookBookSocial/frontend/src/pages/PasswordReset/PasswordReset.jsx
@@ -9,26 +9,37 @@ function PasswordReset() {
   const [showBanner, setShowBanner] = useState(false);
   const [countdown, setCountdown] = useState(5);
   const [showError, setShowError] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [hasStartedTyping, setHasStartedTyping] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    sendPasswordResetEmail(auth, email)
-      .then(() => {
-        setShowBanner(true);
-        const interval = setInterval(() => {
-          setCountdown((prevCountdown) => prevCountdown - 1);
-        }, 1000);
-        setTimeout(() => {
-          clearInterval(interval);
-          window.location.href = "/login";
-        }, 5000);
-      })
-      .catch((error) => {
-        console.error(error);
-        if (error.code === "auth/user-not-found") {
-          setShowError(true);
-        }
-      });
+    setShowButton(false);
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setShowBanner(true);
+      const interval = setInterval(() => setCountdown((prevCountdown) => prevCountdown - 1), 1000);
+      setTimeout(() => {
+        clearInterval(interval);
+        window.location.href = "/login";
+      }, 5000);
+    } catch (error) {
+      console.error(error);
+      if (error.code === "auth/user-not-found") {
+        setShowError(`Email not found. Please check your email address and try again.`);
+      } else if (error.code === "auth/too-many-requests") {
+        setShowError(`Too many requests. Try again later.`);
+      } else {
+        setShowError(`Error sending email. Please try again later.`);
+      }
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setHasStartedTyping(true);
+    setShowError(false);
+    setShowButton(true);
   };
 
   return (
@@ -36,29 +47,26 @@ function PasswordReset() {
       <div className={styles["form-box"]}>
         <h1>Password Reset</h1>
         <form onSubmit={handleSubmit}>
-          <label>
-            <input
-              type="email"
-              placeholder="Email Address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              onInput={() => setShowError(false)}
-            />
-          </label>
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={handleEmailChange}
+            disabled={showBanner}
+          />
           <br />
           <br />
-          <button type="submit">Submit</button>
+          {showButton && <button type="submit">Submit</button>}
         </form>
         {showBanner && (
           <div className={styles.banner}>
-            <p>
-              Password reset email sent. Redirecting to login page.
-            </p>
+            <p>Password reset email sent. Redirecting to login page.</p>
+            <div className={styles.spinner} />
           </div>
         )}
         {showError && (
           <div className={styles.error}>
-            <p>Email not found. Please check your email address and try again.</p>
+            <p>{showError}</p>
           </div>
         )}
       </div>
