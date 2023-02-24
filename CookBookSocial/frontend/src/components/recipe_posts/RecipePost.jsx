00b41e1f7867917669d894f1051df85d3a38ebe8
Terry Pi <tai-jui@umail.ucsb.edu>
2023-02-23 22:54:20 -0800

    tp - remove dropdown for recipes on homepage

--- CookBookSocial/frontend/src/components/recipe_posts/RecipePost.jsx
+++ CookBookSocial/frontend/src/components/recipe_posts/RecipePost.jsx
@@ -62,6 +62,15 @@ function RecipePost({ recipe }) {
             className="bg-white overflow-hidden pb-10 mb-10 border-b border-neutral-300 text-left"
             onClick={toggleShowFull}
         >
+            {currentUser.uid === recipe.uid && (
+                <a
+                    type="button"
+                    class="text-white bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2 mt-4"
+                    href={editPostPath}
+                >
+                    Edit
+                </a>
+            )}
             <header className="header">
                 <h2 className="font-extrabold text-left text-3xl">{displayRecipeTitle(recipe)}</h2>
             </header>
@@ -75,28 +84,6 @@ function RecipePost({ recipe }) {
                     <img src={recipe.image} alt="Error loading image" />
                 </Link>
             </div>
-
-            {showFullRecipe && (
-                <footer>
-                    <div className="ingredients">
-                        <h2 className="ingredients-header">Ingredients</h2>
-                        <ul className="post-list">{renderIngredients(recipe.ingredients)}</ul>
-                    </div>
-                    <div className="instructions">
-                        <h2 className="instructions-header">Instructions</h2>
-                        <ol className="post-list">{renderInstructions()}</ol>
-                    </div>
-                    {currentUser.uid === recipe.uid && (
-                        <a
-                            type="button"
-                            class="text-white bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2 mt-4"
-                            href={editPostPath}
-                        >
-                            Edit
-                        </a>
-                    )}
-                </footer>
-            )}
         </div>
     );
 }
