d66facfab6460df9bf8ffc2c43e1ed471e607760
Nicolas Johnson <njohnson@ucsb.edu>
2023-02-27 20:53:38 -0800

    add backend for sending friend requests

--- CookBookSocial/backend/controllers/userController.js
+++ CookBookSocial/backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import { db } from "../firebase.js";
-import { setDoc, doc, collection, serverTimestamp } from "firebase/firestore";
+import { setDoc, doc, collection, serverTimestamp, getDoc, updateDoc } from "firebase/firestore";
 
 const addUser = async (req, res, next) => {
     try {
@@ -25,4 +25,56 @@ const deleteUser = async (req, res, next) => {
     }
 };
 
-export { addUser, deleteUser };
+const sendFriendRequest = async (req, res, next) => {
+    try{
+        const idSentFrom = req.query.idSent;
+        const idSentTo = req.query.idReceived;
+        const docRefSent = doc(db, "users", idSentFrom);
+        const docRefReceived = doc(db, "users", idSentTo);
+
+        const docSnapSend = await getDoc(docRefSent);
+        const docSnapRec = await getDoc(docRefReceived);
+
+        if(!docSnapSend.exists() || !docSnapRec.exists() ){
+            throw new Error("One of these users does not exist!");
+        }
+
+        const updateDataSend = docSnapSend.data();
+        const updateDataRec = docSnapRec.data();
+
+        if("sentFriendRequests" in updateDataSend){
+            if(updateDataSend['sentFriendRequests'].includes(idSentTo)){
+                throw new Error("Already sent friend request to them.");
+
+            } else {
+                updateDataSend['sentFriendRequests'].push(idSentTo);
+            }
+        } else {
+            updateDataSend['sentFriendRequests'] = [ idSentTo ];
+        }
+
+        if("receivedFriendRequests" in updateDataRec){
+            if(updateDataRec['receivedFriendRequests'].includes(idSentFrom)){
+                throw new Error(" WARNING: They have already received your friend request.  THIS SHOULD NOT HAPPEN");
+            } else {
+                updateDataRec['receivedFriendRequests'].push(idSentFrom);
+            }
+        } else {
+            updateDataRec['receivedFriendRequests'] = [ idSentFrom ];
+        }
+
+        await updateDoc(docRefSent, updateDataSend);
+        await updateDoc(docRefReceived, updateDataRec);
+
+        res.status(200).send(`Success.  Friend Request Sent.`);
+
+
+
+
+    } catch(e){
+        res.status(400).send(`Error: ${e}`);
+        return;
+    }
+}
+
+export { addUser, deleteUser, sendFriendRequest };
