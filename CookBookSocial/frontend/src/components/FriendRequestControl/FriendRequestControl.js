e623a5a7b7a51ae91e8a1cdec63733848a2168d5
Nicolas Johnson <njohnson@ucsb.edu>
2023-03-02 16:42:42 -0800

    nj - refactor the Friend Reuqest control buttons

---
+++ CookBookSocial/frontend/src/components/FriendRequestControl/FriendRequestControl.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import './FriendRequestControl.css'
+import CancelFriendRequest from "../cancelFriendRequest/cancelFriendRequest";
+
+export default function FriendRequestControl({senderId, receiverId, displayName, setReceivedFriendRequests}){
+
+    const URL_GET_USERS_FRIEND_REQUESTS = `/api/user/friend-requests/${receiverId}`;
+
+    function reloadFriendReqs() {
+        fetch(URL_GET_USERS_FRIEND_REQUESTS)
+            .then((response) => response.json())
+            .then((data) => setReceivedFriendRequests(data));
+    }
+
+    function acceptFriend(senderId) {
+        const URL_ACCEPT_FRIEND_REQUEST = `/api/user/friend-accept/${receiverId}/${senderId}`;
+        const response = fetch(URL_ACCEPT_FRIEND_REQUEST, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then(function (data) {
+            console.log(data);
+            reloadFriendReqs();
+        });
+
+        console.log(response);
+
+    }
+
+    async function rejectFriend(senderId) {
+
+        await CancelFriendRequest(receiverId, senderId).then(function (data) {
+            console.log(data);
+            reloadFriendReqs();
+        });
+
+    }
+
+    return( 
+    
+    <div className="friendRequest" key={senderId}>{displayName}
+    <span className="rec-friend-req-btn text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+        onClick={() => acceptFriend(senderId)}
+    >
+        Accept</span>
+
+    <span className="rec-friend-req-btn text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+        onClick={() => rejectFriend(senderId)}
+    >
+        Reject</span>
+
+
+
+
+</div>)
+}
--- CookBookSocial/frontend/src/components/recipe_posts/RecipePost.jsx
+++ CookBookSocial/frontend/src/components/recipe_posts/RecipePost.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useEffect } from 'react';
 
 import { renderIngredients } from "./functions/RecipePostFunctions";
 import { Button } from "react-bootstrap";
