98b6d8c69e42c73ff000c0e9d00e4a09e039d641
Terry Pi <tai-jui@umail.ucsb.edu>
2023-03-02 16:06:26 -0800

    tp - Finished updating nav bar

--- CookBookSocial/frontend/src/pages/ProfilePicPage/ProfilePic.jsx
+++ CookBookSocial/frontend/src/pages/ProfilePicPage/ProfilePic.jsx
@@ -10,125 +10,118 @@ import { db } from "../../config/firebase";
 import { doc, setDoc } from "firebase/firestore";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+    return classes.filter(Boolean).join(" ");
 }
 
 export default function ProfilePic() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [avatars, setAvatars] = useState([]);
-  const [selectedAvatar, setSelectedAvatar] = useState();
-  const [username, setUsername] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [avatars, setAvatars] = useState([]);
+    const [selectedAvatar, setSelectedAvatar] = useState();
+    const [username, setUsername] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  const { currentUser, updateUserProfile, setError } = useAuth();
+    const { currentUser, updateUserProfile, setError } = useAuth();
 
-  useEffect(() => {
-    const fetchData = () => {
-      const res = generateAvatar();
-      setAvatars(res);
+    useEffect(() => {
+        const fetchData = () => {
+            const res = generateAvatar();
+            setAvatars(res);
+        };
+
+        fetchData();
+    }, []);
+
+    const backClick = () => {
+        navigate(-1);
     };
 
-    fetchData();
-  }, []);
+    const picClicked = (index) => {
+        setSelectedAvatar(index);
+        console.log("Clicked: ", index);
+    };
 
-  const backClick = () => {
-    navigate(-1);
-  };
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
 
-  const picClicked = (index) => {
-    setSelectedAvatar(index);
-    console.log("Clicked: ", index);
-  };
+        if (selectedAvatar === undefined) {
+            return setError("Please select an avatar");
+        }
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
+        try {
+            setError("");
+            setLoading(true);
+            const user = currentUser;
+            const profile = {
+                displayName: username ? username : currentUser.displayName,
+                photoURL: avatars[selectedAvatar],
+            };
+            await updateUserProfile(user, profile);
+            //update user's firestore doc with new profile photo and display name
+            const userRef = doc(db, "users", currentUser.uid);
+            setDoc(userRef, { profile: profile }, { merge: true });
+            navigate("/home");
+        } catch (e) {
+            setError("Failed to update profile");
+        }
 
-    if (selectedAvatar === undefined) {
-      return setError("Please select an avatar");
-    }
+        setLoading(false);
+    };
 
-    try {
-      setError("");
-      setLoading(true);
-      const user = currentUser;
-      const profile = {
-        displayName: username ? username : currentUser.displayName,
-        photoURL: avatars[selectedAvatar],
-      };
-      await updateUserProfile(user, profile);
-      //update user's firestore doc with new profile photo and display name
-      const userRef = doc(db, "users", currentUser.uid);
-      setDoc(userRef, { profile: profile }, { merge: true });
-      navigate("/home");
-    } catch (e) {
-      setError("Failed to update profile");
-    }
+    return (
+        <>
+            <button className={styles.backButton} onClick={backClick}>
+                Back
+            </button>
+            <div className="d-flex justify-content-center">
+                <div className="">
+                    <div className="text-center">
+                        <h2 className="mt-4 text-3xl text-center tracking-tight font-light dark:text-white">
+                            Pick an avatar
+                        </h2>
+                    </div>
 
-    setLoading(false);
-  };
+                    <form className="" onSubmit={handleFormSubmit}>
+                        <div className={styles.imagesContainer}>
+                            {avatars.map((avatar, index) => (
+                                <div key={index}>
+                                    <div className={styles.picSize}>
+                                        <img
+                                            alt="gallery"
+                                            src={avatar}
+                                            onClick={() => picClicked(index)}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
 
-  return (
-    <>
-      <button className={styles.backButton} onClick={backClick}>
-        Back
-      </button>
-      <div className="d-flex justify-content-center">
-        <div className="">
-          <div className="text-center">
-            <h2 className="mt-4 text-3xl text-center tracking-tight font-light dark:text-white">
-              Pick an avatar
-            </h2>
-          </div>
+                        <div className={styles.nameField}>
+                            <input
+                                id="username"
+                                name="username"
+                                type="text"
+                                autoComplete="username"
+                                required
+                                className="px-3 py-2 border border-gray-300"
+                                placeholder="Enter a Display Name"
+                                defaultValue={currentUser.displayName && currentUser.displayName}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
 
-          <form className="" onSubmit={handleFormSubmit}>
-            <div className={styles.imagesContainer}>
-              {avatars.map((avatar, index) => (
-                <div key={index}>
-                  <div className={styles.picSize}>
-                    <img
-                      alt="gallery"
-                      className={classNames(
-                        index === selectedAvatar
-                          ? styles.picChosen
-                          : styles.picNotChosen
-                      )}
-                      src={avatar}
-                      onClick={() => picClicked(index)}
-                    />
-                  </div>
+                        <div>
+                            <button
+                                type="submit"
+                                disabled={loading}
+                                className="py-2 px-4 border border-transparent"
+                            >
+                                Update Profile
+                            </button>
+                        </div>
+                    </form>
                 </div>
-              ))}
             </div>
-
-            <div className={styles.nameField}>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="username"
-                required
-                className="px-3 py-2 border border-gray-300"
-                placeholder="Enter a Display Name"
-                defaultValue={
-                  currentUser.displayName && currentUser.displayName
-                }
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                disabled={loading}
-                className="py-2 px-4 border border-transparent"
-              >
-                Update Profile
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
