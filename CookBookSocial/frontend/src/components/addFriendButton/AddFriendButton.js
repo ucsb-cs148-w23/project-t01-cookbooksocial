2085e7d3e8197e5a6d90498f3c8aabdec19d6eea
Nick Johnson <njohnson@ucsb.edu>
2023-03-04 01:09:14 -0800

    nj - add loaders/spinners to all the add friend buttons and also frontend error handling for adding friends

--- CookBookSocial/frontend/src/components/addFriendButton/AddFriendButton.js
+++ CookBookSocial/frontend/src/components/addFriendButton/AddFriendButton.js
@@ -14,6 +14,7 @@ export default function AddFriendButton({ currentUserId, profileUid, profileInfo
     const NOT_FRIENDED = "not friended";
     const IS_CURRENT_USER = "is current user";
     const [friendedState, setFriendedState] = useState(UNKNOWN);
+    const [isButtonLoading, setisButtonLoading] = useState(false);
 
     useEffect(() => {
         
@@ -47,60 +48,79 @@ export default function AddFriendButton({ currentUserId, profileUid, profileInfo
 
     const URL_SEND_FRIEND_REQUEST = `/api/user/friend-request/${currentUserId}/${profileUid}`;
     function addFriend() {
-
-        setFriendedState(REQUEST_SENT);
+        setisButtonLoading(true);
+        
         /*
             currentUser.uid is the user id of the current viewer/user.  userId is the id of the user profile that they are viewing.  Need both id's when making a friend request.  currentUser will have the other user id in their 'sentFriendRequests' list in firebase.  The profile being viewed will have the current user ID in their 'receivedFriendRequests' list in firebase.
         */
 
-        const response = fetch(URL_SEND_FRIEND_REQUEST, {
+        fetch(URL_SEND_FRIEND_REQUEST, {
             method: 'PUT',
             headers: {
                 'Content-type': 'application/json'
             }
         }).then(function (data) {
-            console.log(data);
+                setisButtonLoading(false);
+            if(data.status === 200){
+                setFriendedState(REQUEST_SENT);
+            }
+            
         });
 
-        console.log(response);
 
     }
 
     function acceptFriend() {
+        setisButtonLoading(true);
         const URL_ACCEPT_FRIEND_REQUEST = `/api/user/friend-accept/${currentUserId}/${profileUid}`;
-        setFriendedState(FRIEND);
-        const response = fetch(URL_ACCEPT_FRIEND_REQUEST, {
+        fetch(URL_ACCEPT_FRIEND_REQUEST, {
             method: 'PUT',
             headers: {
                 'Content-type': 'application/json'
             }
         }).then(function (data) {
-            console.log(data);
-
+                setisButtonLoading(false);
+            if(data.status === 200){
+                setFriendedState(FRIEND);
+            } 
         });
-
-        console.log(response);
 
     }
 
     function rejectFriend() {
-        setFriendedState(NOT_FRIENDED);
-        CancelFriendRequest(currentUserId, profileUid);
+        setisButtonLoading(true);
+        CancelFriendRequest(currentUserId, profileUid)
+        .then(function (data) {
+            setisButtonLoading(false);
+            if (data.status === 200) {
+                setFriendedState(NOT_FRIENDED);
+            }
+        });
 
     }
 
     function cancelRequest(){
-        setFriendedState(NOT_FRIENDED);
-        CancelFriendRequest(profileUid, currentUserId);
+        setisButtonLoading(true);
+        CancelFriendRequest(profileUid, currentUserId)
+            .then(function (data) {
+                setisButtonLoading(false);
+                if (data.status === 200) {
+                    setFriendedState(NOT_FRIENDED);
+                }
+            });
     }
 
     if(friendedState === NOT_FRIENDED){
         return (
-            <span
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                onClick={() => addFriend()}>
+            <button
+                className="friend-buttons bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                onClick={() => addFriend()}
+                disabled={isButtonLoading}>
                 Add Friend
-            </span>
+                {isButtonLoading && (
+                    <div className="friend-button-loader"> </div>
+                )}
+            </button>
         )
     } else if (friendedState === UNKNOWN){
         return (
@@ -109,26 +129,35 @@ export default function AddFriendButton({ currentUserId, profileUid, profileInfo
             </span>
         )
     } else if (friendedState === REQUEST_RECEIVED){
-        return (
-            <span>
-                <span className="rec-friend-req-btn text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
-                    onClick={() => acceptFriend()}
-                >
-                    Accept Friend Request</span>
+        if(!isButtonLoading){
+            return (
+                <span>
+                    <span className="rec-friend-req-btn text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+                        onClick={() => acceptFriend()}
+                    >
+                        Accept Friend Request</span>
                     <span className="rec-friend-req-btn text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-                    onClick={() => rejectFriend()}
-                >
-                    Reject</span>
-            </span>
-        )
+                        onClick={() => rejectFriend()}
+                    >
+                        Reject</span>
+                </span>
+            )
+        } else {
+            return (<div className="friend-request-loader"> </div>)
+        }
+
     } else if (friendedState === REQUEST_SENT){
         return (
 
-                <div className="rec-friend-cancel-btn text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-2 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                <button className="friend-buttons rec-friend-cancel-btn text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-2 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                 onClick={() => cancelRequest()}
+                disabled={isButtonLoading}
                 >
                     Cancel Friend Request
-               </div>
+                {isButtonLoading && (
+                    <div className="friend-button-loader"> </div>
+                )}
+               </button>
         )
     } else if (friendedState === FRIEND){
         return (
@@ -140,7 +169,7 @@ export default function AddFriendButton({ currentUserId, profileUid, profileInfo
 
         )
     } else if (friendedState === IS_CURRENT_USER){
-        return(<span></span>);
+        return(<></>);
     }
 
 }
