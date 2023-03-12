9b404eea9a081f8fc2fbfe6180e6f2b01fccb9ee
bzamora020 <bryan.aldair.zamora@gmail.com>
2023-03-12 13:54:38 -0700

    stage changes

--- CookBookSocial/frontend/src/pages/RecipeListPage/RecipeListPage.jsx
+++ CookBookSocial/frontend/src/pages/RecipeListPage/RecipeListPage.jsx
@@ -1,27 +1,39 @@
-import { collection, doc, getFirestore } from "firebase/firestore";
-import React, { useEffect } from "react";
+import { collection, doc, getDoc, getFirestore } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RecipeList from "../../components/RecipeLists/RecipeList/RecipeList";
 
 function RecipeListPage() {
+  const [recipeLists, setRecipeLists] = useState([]);
+
   const id = useParams();
 
   useEffect(() => {
     const db = getFirestore();
     const userReference = doc(collection(db, "users"), id);
     getDoc(userReference).then((doc) => {
-        const data = doc.data();
-        
-
-
+      const data = doc.data();
+      if (data["recipeLists"]) {
+        setRecipeLists(data["recipeLists"]);
+      }
     });
   });
+
+  const renderRecipes = () => {
+    if (recipeLists.length === 0)
+    {
+      return <div>No Recipe lists</div>
+    }
+    for(let i = 0; i < recipeLists.length; i++) {
+      
+    }
+  }
 
   return (
     <>
       <div>RecipeListPage</div>
 
-      <RecipeList />
+      <RecipeList recipes={recipe}/>
     </>
   );
 }
