90722387144231674d6afdcb82d1f28ab4a835bd
bzamora020 <bryan.aldair.zamora@gmail.com>
2023-03-15 14:32:31 -0700

    login errors fixed

--- CookBookSocial/frontend/src/pages/LoginPage/Login.jsx
+++ CookBookSocial/frontend/src/pages/LoginPage/Login.jsx
@@ -2,120 +2,137 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 import { useAuth } from "../../contexts/AuthContext";
+import { db } from "../../config/firebase";
+
+import { doc, getDoc } from "firebase/firestore";
+
 import styles from "./Login.module.css";
 
 export default function Login() {
-    const { currentUser, login, setError } = useAuth();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
+  const { currentUser, login, setError } = useAuth();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        currentUser && navigate("/home");
-    }, [currentUser, navigate]);
-
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-        setError("");
-        setLoading(true);
-        try {
-            await login(email, password);
+  useEffect(() => {
+    if (currentUser) {
+      const userInfoRef = doc(db, "users", currentUser.uid);
+      getDoc(userInfoRef)
+        .then((snapshot) => {
+          // If the user does not already have user data, we redirect them to the edit-profile
+          if (!snapshot.exists()) {
+            navigate("/edit-profile");
+          } else {
+            // Otherwise, their profile is good, so we send them to home
             navigate("/home");
-        } catch (e) {
-            setError("Failed to login");
-        }
-        setLoading(false);
-    };
+          }
+        })
+        .catch((error) => console.log(error.message));
+    }
+  }, [currentUser, navigate]);
 
-    const GoogleRedirect = () => signInWithRedirect(auth, provider);
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
-    return (
-        <div className={styles.container}>
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    try {
+      await login(email, password);
+      navigate("/home");
+    } catch (e) {
+      setError("Failed to login");
+    }
+    setLoading(false);
+  };
 
-            <div className={styles.slider}>
-                <div className={styles.slide}></div>
-                <div className={styles.slide}></div>
-                <div className={styles.slide}></div>
-                <div className={styles.slide}></div>
-                <div className={styles.slide}></div>
-            </div>
+  const GoogleRedirect = () => signInWithRedirect(auth, provider);
 
-            <div className={styles.rectangle}>
-                <div className={styles.topText}>
-                    <h2>Login to your account</h2>
-                </div>
-                <form onSubmit={handleFormSubmit}>
-                    <div>
-                        <div className={styles.inputFields}>
-                            <label htmlFor="email-address"> </label>
-                            <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
-                                placeholder="Email Address"
-                                required
-                                className={styles.input}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.slider}>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+      </div>
 
-                        <div className={styles.inputFields}>
-                            <label htmlFor="password"></label>
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                placeholder="Password"
-                                autoComplete="current-password"
-                                required
-                                className={styles.input}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    <div className={styles.inputFields}>
-                        <button type="submit" disabled={loading} className={styles.submit_button}>
-                            Login
-                        </button>
-                    </div>
-                </form>
-
-                <div className={styles.inputFields}>
-                    <div className={styles.orText}>Or sign in with:</div>
-                    <button
-                        onClick={GoogleRedirect}
-                        disabled={loading}
-                        className={styles.googleButton}
-                    >
-                        <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                            alt="Google logo"
-                            className={styles.googleIcon}
-                        />
-                    </button>
-                </div>
-                <div className="flex items-center justify-between"></div>
-                <div className="text-sm">
-                    <Link to="/password-reset" className="text-blue-600">
-                        Forgot Password?
-                    </Link>
-                </div>
-
-                <div className="text-sm">
-                    <Link to="/register" className="text-blue-600">
-                        Don't have an account? Register
-                    </Link>
-                </div>
-            </div>
-
-
-            
+      <div className={styles.rectangle}>
+        <div className={styles.topText}>
+          <h2>Login to your account</h2>
         </div>
-    );
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <div className={styles.inputFields}>
+              <label htmlFor="email-address"> </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                placeholder="Email Address"
+                required
+                className={styles.input}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className={styles.inputFields}>
+              <label htmlFor="password"></label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+                required
+                className={styles.input}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className={styles.inputFields}>
+            <button
+              type="submit"
+              disabled={loading}
+              className={styles.submit_button}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        <div className={styles.inputFields}>
+          <div className={styles.orText}>Or sign in with:</div>
+          <button
+            onClick={GoogleRedirect}
+            disabled={loading}
+            className={styles.googleButton}
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="Google logo"
+              className={styles.googleIcon}
+            />
+          </button>
+        </div>
+        <div className="flex items-center justify-between"></div>
+        <div className="text-sm">
+          <Link to="/password-reset" className="text-blue-600">
+            Forgot Password?
+          </Link>
+        </div>
+
+        <div className="text-sm">
+          <Link to="/register" className="text-blue-600">
+            Don't have an account? Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
