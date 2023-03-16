0072b1c2abce1ed62955d48561f23459b7b2e036
Nicolas Johnson <njohnson@ucsb.edu>
2023-03-16 16:42:09 -0700

    finished notifiations with main merged into it

--- CookBookSocial/frontend/src/components/Navbar/Navbar.jsx
+++ CookBookSocial/frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 import SearchBar from "../Search/Search";
 
 import { useAuth } from "../../contexts/AuthContext";
