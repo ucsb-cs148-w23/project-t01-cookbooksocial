0575984c2c415f5b8176494c531d1dcd4ef4faea
Jacob Toomey <jtoomey@ucsb.edu>
2023-03-17 21:09:03 -0700

    Revert "Get rid of duplicate delete and edit buttons on recipe page"
    This reverts commit f616c38317e160b24b1e231cf94dcb59843718b0.

--- CookBookSocial/frontend/src/pages/RecipePage/RecipePage.jsx
+++ CookBookSocial/frontend/src/pages/RecipePage/RecipePage.jsx
@@ -314,7 +314,18 @@ function RecipePage() {
                 comments={commentsArr}
                 onMount={handleCommentsMount}
             />
-
+            {currentUser.uid === recipe.uid && (
+                <div>
+                    <a
+                        type="button"
+                        className="text-white bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2 mt-4"
+                        href={editPostPath}
+                    >
+                        Edit
+                    </a>
+                    <DeleteButton recipeId={recipeId}></DeleteButton>
+                </div>
+            )}
         </>
     );
 }
